0a2aa4c3d94ddb53bad41544fd21ac8c2a4d6d51
hyun_todolist <exit7101c@gmail.com>
2024-02-06 22:45:41 +0900

    로그인화면 ui 구현

---
+++ Hyun_TodoList/Views/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  Hyun_TodoList
+//
+//  Created by hyun on 2/6/24.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
