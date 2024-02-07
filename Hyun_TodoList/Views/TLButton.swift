af97f6bd025841cfde5772fd74831708ae4135f2
hyun_todolist <exit7101c@gmail.com>
2024-02-07 23:51:52 +0900

    등록화면 ui 변경 , 등록화면및 로그인화면 통합버튼 생성 .

---
+++ Hyun_TodoList/Views/TLButton.swift
@@ -0,0 +1,18 @@
+//
+//  TLButton.swift
+//  Hyun_TodoList
+//
+//  Created by hyun on 2/7/24.
+//
+
+import SwiftUI
+
+struct TLButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TLButton()
+}
